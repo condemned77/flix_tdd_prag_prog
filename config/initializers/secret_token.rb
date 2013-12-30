0c8dc6797c5ea5d1e5981c9b2eb32efd99439328
mjaeger <huNt@h.multi.box>
2013-12-30 22:08:51 +0100

    Project setup for tdd version of flix example.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlixTdd::Application.config.secret_key_base = '91bf14d8ea3ac49eacf1d9fac59f390aa2462cd2aac2934b24083217b9d34bb071b7505ab52e6c99387e1ab269e4a3dc83831f65656a64986f6176b4429817db'
