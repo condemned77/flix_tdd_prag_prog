0cba6f8f35edc1165bfe446787a382a3244567bc
mjaeger <huNt@h.local>
2014-02-16 15:40:38 +0100

    Finished TDD exercise up to: Show a movie. Specs can not be executed from shell, only from within vim. Executing rspec from shell shows:
    ***/.rvm/gems/ruby-2.0.0-p353/gems/rspec-core-2.13.1/lib/rspec/core/configuration.rb:819:in
    `load': cannot load such file --
    ***/RailsStuff/PragStudio/flix_tdd/spec/models/spec (LoadError)
    from
    ***/.rvm/gems/ruby-2.0.0-p353/gems/rspec-core-2.13.1/lib/rspec/core/configuration.rb:819:in
    `block in load_spec_files'
    from
    ***/.rvm/gems/ruby-2.0.0-p353/gems/rspec-core-2.13.1/lib/rspec/core/configuration.rb:819:in
    `each'
    from
    ***/.rvm/gems/ruby-2.0.0-p353/gems/rspec-core-2.13.1/lib/rspec/core/configuration.rb:819:in
    `load_spec_files'
    from
    ***/.rvm/gems/ruby-2.0.0-p353/gems/rspec-core-2.13.1/lib/rspec/core/command_line.rb:22:in
    `run'
    from
    ***/.rvm/gems/ruby-2.0.0-p353/gems/rspec-core-2.13.1/lib/rspec/core/runner.rb:80:in
    `run'
    from
    ***/.rvm/gems/ruby-2.0.0-p353/gems/rspec-core-2.13.1/lib/rspec/core/runner.rb:17:in
    `block in autorun'

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,8 @@ FlixTdd::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  get "movies" => "movies#index"
+  get "movies/:id" => "movies#show", as: "movie"
+
 end
